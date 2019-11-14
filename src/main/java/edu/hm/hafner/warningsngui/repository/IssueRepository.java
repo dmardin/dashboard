ed0e895cf133eb647384260457ba839ae0745abc
dmardin <deniz.mardin@gmx.de>
2019-11-14 11:29:02 +0100

    Remove currently not needed repositories

--- src/main/java/edu/hm/hafner/warningsngui/repository/IssueRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.hm.hafner.warningsngui.repository;
-
-import edu.hm.hafner.warningsngui.model.IssueEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueRepository extends JpaRepository<IssueEntity, Integer> {
-}
