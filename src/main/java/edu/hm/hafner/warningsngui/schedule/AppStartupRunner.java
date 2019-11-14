6bede31a3f37339f0d47b6f75728f2d10652021d
dmardin <deniz.mardin@gmx.de>
2019-11-14 11:03:05 +0100

    clean up AppStartupRunner

--- src/main/java/edu/hm/hafner/warningsngui/schedule/AppStartupRunner.java
+++ src/main/java/edu/hm/hafner/warningsngui/schedule/AppStartupRunner.java
@@ -34,71 +34,69 @@ public class AppStartupRunner implements ApplicationRunner {
         List<Job> allJobs = new ArrayList<>();
         JobsResponse jobsResponse = restService.getProjects();
         for (Job job : jobsResponse.getJobs()) {
-            if (job.getName().contains("plagi")) {
 
-                //Get Builds for every Job form Jenkins
-                BuildsResponse buildsResponse = restService.getBuilds(job.getUrl() + API_JSON);
-                for (Build build : buildsResponse.getBuilds()) {
-                    if (job.getBuilds() != null) {
-                        job.getBuilds().add(build);
-                        build.setJob(job);
-                    }
+            //Get Builds for every Job form Jenkins
+            BuildsResponse buildsResponse = restService.getBuilds(job.getUrl() + API_JSON);
+            for (Build build : buildsResponse.getBuilds()) {
+                if (job.getBuilds() != null) {
+                    job.getBuilds().add(build);
+                    build.setJob(job);
+                }
 
-                    //Get used Tools for every Build form Jenkins
-                    ToolsResponse toolsResponse = restService.getTools(build.getUrl() + WARNINGS + SLASH + API_JSON);
-                    if (toolsResponse != null) {
-                        Tool[] tools = toolsResponse.getTools();
-                        for (Tool tool : tools) {
+                //Get used Tools for every Build form Jenkins
+                ToolsResponse toolsResponse = restService.getTools(build.getUrl() + WARNINGS + SLASH + API_JSON);
+                if (toolsResponse != null) {
+                    Tool[] tools = toolsResponse.getTools();
+                    for (Tool tool : tools) {
 
-                            ToolDetailResponse toolDetailResponse = restService.getToolsDetail(build.getUrl() + tool.getId().toLowerCase() + SLASH + API_JSON);
+                        ToolDetailResponse toolDetailResponse = restService.getToolsDetail(build.getUrl() + tool.getId().toLowerCase() + SLASH + API_JSON);
 
-                            ResultEntity result = new ResultEntity();
-                            build.getResultEntities().add(result);
+                        ResultEntity result = new ResultEntity();
+                        build.getResultEntities().add(result);
 
-                            result.setName(tool.getName());
-                            result.setTotal(result.getTotal() + tool.getSize());
-                            result.setBuild(build);
+                        result.setName(tool.getName());
+                        result.setTotal(result.getTotal() + tool.getSize());
+                        result.setBuild(build);
 
-                            List<ErrorMessage> errorMessages = new ArrayList<>();
-                            for (String err : toolDetailResponse.getErrorMessages()) {
-                                ErrorMessage em = new ErrorMessage();
-                                em.setMessage(err);
-                                em.setResult(result);
-                                errorMessages.add(em);
-                            }
-                            result.setErrorMessages(errorMessages);
+                        List<ErrorMessage> errorMessages = new ArrayList<>();
+                        for (String err : toolDetailResponse.getErrorMessages()) {
+                            ErrorMessage em = new ErrorMessage();
+                            em.setMessage(err);
+                            em.setResult(result);
+                            errorMessages.add(em);
+                        }
+                        result.setErrorMessages(errorMessages);
 
-                            List<InfoMessage> infoMessages = new ArrayList<>();
-                            for (String info : toolDetailResponse.getInfoMessages()) {
-                                InfoMessage im = new InfoMessage();
-                                im.setMessage(info);
-                                im.setResult(result);
-                                infoMessages.add(im);
-                            }
+                        List<InfoMessage> infoMessages = new ArrayList<>();
+                        for (String info : toolDetailResponse.getInfoMessages()) {
+                            InfoMessage im = new InfoMessage();
+                            im.setMessage(info);
+                            im.setResult(result);
+                            infoMessages.add(im);
+                        }
 
-                            result.setInfoMessages(infoMessages);
-                            result.setFixedSize(toolDetailResponse.getFixedSize());
-                            result.setNewSize(toolDetailResponse.getNewSize());
-                            result.setQualityGateStatus(toolDetailResponse.getQualityGateStatus());
-                            result.setTotalSize(toolDetailResponse.getTotalSize());
+                        result.setInfoMessages(infoMessages);
+                        result.setFixedSize(toolDetailResponse.getFixedSize());
+                        result.setNewSize(toolDetailResponse.getNewSize());
+                        result.setQualityGateStatus(toolDetailResponse.getQualityGateStatus());
+                        result.setTotalSize(toolDetailResponse.getTotalSize());
 
-                            for (WarningType warningType : WarningType.values()) {
-                                IssuesEntity issuesEntity = new IssuesEntity();
-                                issuesEntity.setResultEntity(result);
-                                result.getIssues().add(issuesEntity);
-                                issuesEntity.setWarningType(warningType);
-                                String url = tool.getLatestUrl() + SLASH + warningType.toString().toLowerCase() + SLASH + API_JSON;
-                                IssuesResponse issuesResponse = restService.getIssues(url);
-                                if (issuesResponse != null) {
-                                    IssueEntity[] issues = issuesResponse.getIssues();
-                                    for (IssueEntity issue : issues) {
-                                        issuesEntity.getIssues().add(issue);
-                                        issue.setIssues(issuesEntity);
-                                    }
-
+                        for (WarningType warningType : WarningType.values()) {
+                            IssuesEntity issuesEntity = new IssuesEntity();
+                            issuesEntity.setResultEntity(result);
+                            result.getIssues().add(issuesEntity);
+                            issuesEntity.setWarningType(warningType);
+                            String url = tool.getLatestUrl() + SLASH + warningType.toString().toLowerCase() + SLASH + API_JSON;
+                            IssuesResponse issuesResponse = restService.getIssues(url);
+                            if (issuesResponse != null) {
+                                IssueEntity[] issues = issuesResponse.getIssues();
+                                for (IssueEntity issue : issues) {
+                                    issuesEntity.getIssues().add(issue);
+                                    issue.setIssues(issuesEntity);
                                 }
 
                             }
+
                         }
                     }
                 }
