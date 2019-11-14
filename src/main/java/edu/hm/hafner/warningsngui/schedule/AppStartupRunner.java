8a25c5e7ef60b684ff42d1ce48524e7b559c871c
dmardin <deniz.mardin@gmx.de>
2019-11-14 10:58:02 +0100

    cleanup AppStartupRunner

--- src/main/java/edu/hm/hafner/warningsngui/schedule/AppStartupRunner.java
+++ src/main/java/edu/hm/hafner/warningsngui/schedule/AppStartupRunner.java
@@ -1,12 +1,11 @@
 package edu.hm.hafner.warningsngui.schedule;
 
-import edu.hm.hafner.analysis.IssueBuilder;
-import edu.hm.hafner.analysis.Severity;
 import edu.hm.hafner.warningsngui.model.*;
-import edu.hm.hafner.warningsngui.repository.IssueRepository;
 import edu.hm.hafner.warningsngui.repository.JobRepository;
 import edu.hm.hafner.warningsngui.rest.RestService;
 import edu.hm.hafner.warningsngui.rest.response.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -24,19 +23,21 @@ public class AppStartupRunner implements ApplicationRunner {
     @Autowired
     private JobRepository jobRepository;
 
-    @Autowired
-    private IssueRepository issueRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final String SLASH = "/";
+    private static final String API_JSON = "api/json";
+    private static final String WARNINGS = "warnings-ng";
 
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("Start getting Data");
+        logger.info("Start requesting data from Jenkins");
         List<Job> allJobs = new ArrayList<>();
         JobsResponse jobsResponse = restService.getProjects();
         for (Job job : jobsResponse.getJobs()) {
             if (job.getName().contains("plagi")) {
 
                 //Get Builds for every Job form Jenkins
-                BuildsResponse buildsResponse = restService.getBuilds(job.getUrl() + "api/json");
+                BuildsResponse buildsResponse = restService.getBuilds(job.getUrl() + API_JSON);
                 for (Build build : buildsResponse.getBuilds()) {
                     if (job.getBuilds() != null) {
                         job.getBuilds().add(build);
@@ -44,106 +45,60 @@ public class AppStartupRunner implements ApplicationRunner {
                     }
 
                     //Get used Tools for every Build form Jenkins
-                    ToolsResponse toolsResponse = restService.getTools(build.getUrl() + "warnings-ng/" + "api/json");
+                    ToolsResponse toolsResponse = restService.getTools(build.getUrl() + WARNINGS + SLASH + API_JSON);
                     if (toolsResponse != null) {
                         Tool[] tools = toolsResponse.getTools();
                         for (Tool tool : tools) {
 
-                            ToolDetailResponse toolDetailResponse = restService.getToolsDetail(build.getUrl() + tool.getId().toLowerCase() + "/api/json");
+                            ToolDetailResponse toolDetailResponse = restService.getToolsDetail(build.getUrl() + tool.getId().toLowerCase() + SLASH + API_JSON);
 
-//                            if (tool.getId().contains("style")) {
-//                                if (build.getTools() != null) {
-//                                    build.getTools().add(tool);
-//                                    tool.setBuild(build);
-//                                }
+                            ResultEntity result = new ResultEntity();
+                            build.getResultEntities().add(result);
 
-                                ResultEntity result = new ResultEntity();
-                                build.getResultEntities().add(result);
-                                //result.setTool(tool);
-                                result.setName(tool.getName());
-                                result.setTotal(result.getTotal() + tool.getSize());
-                                result.setBuild(build);
+                            result.setName(tool.getName());
+                            result.setTotal(result.getTotal() + tool.getSize());
+                            result.setBuild(build);
 
-                                List<ErrorMessage> errorMessages = new ArrayList<>();
-                                for(String err : toolDetailResponse.getErrorMessages()) {
-                                    ErrorMessage em = new ErrorMessage();
-                                    em.setMessage(err);
-                                    em.setResult(result);
-                                    errorMessages.add(em);
-                                }
-                                result.setErrorMessages(errorMessages);
+                            List<ErrorMessage> errorMessages = new ArrayList<>();
+                            for (String err : toolDetailResponse.getErrorMessages()) {
+                                ErrorMessage em = new ErrorMessage();
+                                em.setMessage(err);
+                                em.setResult(result);
+                                errorMessages.add(em);
+                            }
+                            result.setErrorMessages(errorMessages);
 
-                                List<InfoMessage> infoMessages = new ArrayList<>();
-                                for(String info : toolDetailResponse.getInfoMessages()) {
-                                    InfoMessage im = new InfoMessage();
-                                    im.setMessage(info);
-                                    im.setResult(result);
-                                    infoMessages.add(im);
-                                }
-                                result.setInfoMessages(infoMessages);
+                            List<InfoMessage> infoMessages = new ArrayList<>();
+                            for (String info : toolDetailResponse.getInfoMessages()) {
+                                InfoMessage im = new InfoMessage();
+                                im.setMessage(info);
+                                im.setResult(result);
+                                infoMessages.add(im);
+                            }
 
-                                //result.setErrorMessages(toolsDetailPayload.getErrorMessages());
-                                //result.setInfoMessages(toolsDetailPayload.getInfoMessages());
-                                result.setFixedSize(toolDetailResponse.getFixedSize());
-                                result.setNewSize(toolDetailResponse.getNewSize());
-                                result.setQualityGateStatus(toolDetailResponse.getQualityGateStatus());
-                                result.setTotalSize(toolDetailResponse.getTotalSize());
+                            result.setInfoMessages(infoMessages);
+                            result.setFixedSize(toolDetailResponse.getFixedSize());
+                            result.setNewSize(toolDetailResponse.getNewSize());
+                            result.setQualityGateStatus(toolDetailResponse.getQualityGateStatus());
+                            result.setTotalSize(toolDetailResponse.getTotalSize());
 
-
-//                                List<IssueType> issueTypes = new ArrayList<>();
-//                                issueTypes.add(IssueType.FIXED);
-//                                issueTypes.add(IssueType.NEW);
-//                                issueTypes.add(IssueType.OUTSTANDING);
-
-//                                Report fixed = new Report();
-//                                Report out = new Report();
-//                                Report newW = new Report();
-                                //Map<WarningType, IssueEntity> warnings = new LinkedHashMap<WarningType, IssueEntity>();
-
-                                for (WarningType warningType : WarningType.values()) {
-                                    IssuesEntity issuesEntity = new IssuesEntity();
-                                    issuesEntity.setResultEntity(result);
-                                    result.getIssues().add(issuesEntity);
-                                    issuesEntity.setWarningType(warningType);
-                                    String url = tool.getLatestUrl() + "/" + warningType.toString().toLowerCase() + "/api/json";
-                                    IssuesResponse issuesResponse = restService.getIssues(url);
-                                    if (issuesResponse != null) {
-                                        IssueEntity[] issues = issuesResponse.getIssues();
-                                        for (IssueEntity issue : issues) {
-                                            issuesEntity.getIssues().add(issue);
-                                            issue.setIssues(issuesEntity);
-                                            //TODO issues EntityRepository fehlt !!!!
-                                            //issueRepository.save(issue);
-
-                                            //warnings.put(warningType, issue);
-                                            //
-                                            //set issue Type!!!
-
-//                                            if(issueType == IssueType.FIXED){
-//                                                fixed.add(convertIssue(issue));
-//                                            } else if (issueType == IssueType.NEW) {
-//                                                newW.add(convertIssue(issue));
-//                                            } else if (issueType == IssueType.OUTSTANDING) {
-//                                                out.add(convertIssue(issue));
-//                                            }
-
-
-                                            //tool.getIssues().add(issue);
-                                            //issue.setTool(tool);
-
-                                        }
-                                        //ArrayList list = new ArrayList<>(Arrays.asList(issues));
-                                        //result.setIssues(list);
+                            for (WarningType warningType : WarningType.values()) {
+                                IssuesEntity issuesEntity = new IssuesEntity();
+                                issuesEntity.setResultEntity(result);
+                                result.getIssues().add(issuesEntity);
+                                issuesEntity.setWarningType(warningType);
+                                String url = tool.getLatestUrl() + SLASH + warningType.toString().toLowerCase() + SLASH + API_JSON;
+                                IssuesResponse issuesResponse = restService.getIssues(url);
+                                if (issuesResponse != null) {
+                                    IssueEntity[] issues = issuesResponse.getIssues();
+                                    for (IssueEntity issue : issues) {
+                                        issuesEntity.getIssues().add(issue);
+                                        issue.setIssues(issuesEntity);
                                     }
 
-                                    //result.setWarnings(warnings);
                                 }
-                                //result.setFixedIssuesReference(fixed);
-                                //result.setNewIssuesReference(newW);
-                                //result.setOutstandingIssuesReference(out);
 
-
-//                            }
+                            }
                         }
                     }
                 }
@@ -151,29 +106,6 @@ public class AppStartupRunner implements ApplicationRunner {
             allJobs.add(job);
         }
         jobRepository.saveAll(allJobs);
-        System.out.println("Finitooooooooooooooooooooooooooooooooooooooooooooooo");
-    }
-
-
-    private edu.hm.hafner.analysis.Issue convertIssue(IssueEntity issue) {
-        IssueBuilder issueBuilder = new IssueBuilder();
-        //TODO Linerange
-        return issueBuilder
-                .setCategory(issue.getCategory())
-                .setColumnEnd(issue.getColumnEnd())
-                .setColumnStart(issue.getColumnStart())
-                .setDescription(issue.getDescription())
-                .setFileName(issue.getFileName())
-                .setFingerprint(issue.getFingerprint())
-                .setLineEnd(issue.getLineEnd())
-                .setLineStart(issue.getLineStart())
-                .setMessage(issue.getMessage())
-                .setModuleName(issue.getModuleName())
-                .setOrigin(issue.getOrigin())
-                .setPackageName(issue.getPackageName())
-                .setReference(issue.getReference())
-                .setSeverity(Severity.guessFromString(issue.getSeverity()))
-                .setType(issue.getType())
-                .build();
+        logger.info("Requested data saved to database");
     }
 }
