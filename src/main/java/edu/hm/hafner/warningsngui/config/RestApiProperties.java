d9d1177b7b9aa56f6938376429d93fefa75d97e2
dmardin <deniz.mardin@gmx.de>
2020-02-02 23:07:45 +0100

    Add documentation to config files

--- src/main/java/edu/hm/hafner/warningsngui/config/RestApiProperties.java
+++ src/main/java/edu/hm/hafner/warningsngui/config/RestApiProperties.java
@@ -3,6 +3,12 @@ package edu.hm.hafner.warningsngui.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration to get access to the REST Endpoint from Jenkins.
+ * The Endpoint is specified in the applications.properties of this Project.
+ *
+ * @author Deniz Mardin
+ */
 @Configuration
 @ConfigurationProperties("jenkins.api")
 public class RestApiProperties {
@@ -10,22 +16,45 @@ public class RestApiProperties {
     //TODO add security
     private RestConfiguration restConfiguration;
 
+    /**
+     * Getter for the {@link RestConfiguration}.
+     *
+     * @return the configuration for the REST Endpoint
+     */
     public RestConfiguration getRestConfiguration() {
         return restConfiguration;
     }
 
+    /**
+     * Setter for the {@link RestConfiguration}.
+     *
+     * @param restConfiguration the new configuration for the REST Endpoint
+     */
     public void setRestConfiguration(RestConfiguration restConfiguration) {
         this.restConfiguration = restConfiguration;
     }
 
+    /**
+     * Configuration to get access to the REST Endpoint from Jenkins.
+     */
     public static class RestConfiguration {
 
         private String endPoint;
 
+        /**
+         * Getter for the specified Endpoint.
+         *
+         * @return the Endpoint
+         */
         public String getEndPoint() {
             return endPoint;
         }
 
+        /**
+         * Setter for the specified Endpoint.
+         *
+         * @param endPoint the new Endpoint
+         */
         public void setEndPoint(String endPoint) {
             this.endPoint = endPoint;
         }
--- src/main/java/edu/hm/hafner/warningsngui/config/WarningsNgUiConfig.java
+++ src/main/java/edu/hm/hafner/warningsngui/config/WarningsNgUiConfig.java
@@ -5,9 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration to specify the Messages of the Application.
+ *
+ * @author Deniz Mardin
+ */
 @Configuration
 public class WarningsNgUiConfig implements WebMvcConfigurer {
 
+    /**
+     * Specify the location of the Messages file for this application (messages/content) in the resource folder.
+     *
+     * @return the configured {@link ResourceBundleMessageSource}
+     */
     @Bean
     public ResourceBundleMessageSource messageSource() {
         final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
