f0a97efd7120a4f982dcf078b2e0f10ce24ea963
dmardin <deniz.mardin@gmx.de>
2020-02-09 21:15:49 +0100

    determine easier jobName in js

--- src/main/resources/static/js/aggregated-analysis-results.js
+++ src/main/resources/static/js/aggregated-analysis-results.js
@@ -1,10 +1,8 @@
 $(document).ready(
     function () {
-        const parts = window.location.href.split('/');
-        //TODO doppelter pop - kann man /build nicht in der url einfach entfernen?
-        parts.pop();
-        const lastSegment = parts.pop() || parts.pop();
-        $.get("/ajax/aggregatedAnalysisResults/"+lastSegment/*, {origin: origin, reference: reference}*/,
+        const urlParts = window.location.href.split('/');
+        const jobName = urlParts[urlParts.length - 2];
+        $.get("/ajax/aggregatedAnalysisResults/"+jobName/*, {origin: origin, reference: reference}*/,
             function (builds) {
                 renderTrendChart('aggregated-analysis-results-history-chart', builds, "");
             });
