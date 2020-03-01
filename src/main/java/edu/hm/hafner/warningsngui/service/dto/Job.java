73b2e222a997893585efb2ebb1a319496cdc42d9
dmardin <deniz.mardin@gmx.de>
2020-03-01 08:52:34 +0100

    Add doc to dto's

--- src/main/java/edu/hm/hafner/warningsngui/service/dto/Job.java
+++ src/main/java/edu/hm/hafner/warningsngui/service/dto/Job.java
@@ -3,6 +3,9 @@ package edu.hm.hafner.warningsngui.service.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * DTO that represents a {@link Job}.
+ */
 public class Job {
     private int id;
     private String name;
@@ -11,6 +14,21 @@ public class Job {
     private String lastBuildStatus;
     private List<Build> builds;
 
+    /**
+     * Creates a new instance of a {@link Job}.
+     */
+    Job() {
+        this.builds = new ArrayList<>();
+    }
+
+    /**
+     * Creates a new instance of a {@link Job}.
+     *
+     * @param id the id of the {@link Job}
+     * @param name the name of the {@link Job}
+     * @param url the url of the {@link Job}
+     * @param lastBuildStatus the last build status of the {@link Job}
+     */
     public Job(int id, String name, String url, String lastBuildStatus) {
         this.id = id;
         this.name = name;
@@ -19,58 +37,120 @@ public class Job {
         this.builds = new ArrayList<>();
     }
 
-    Job() {
-        this.builds = new ArrayList<>();
-    }
-
+    /**
+     * Returns the name of the {@link Job}.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter to set the name of the {@link Job}.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the url of the {@link Job}.
+     *
+     * @return the url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Setter to set the url of the {@link Job}.
+     *
+     * @param url the url
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * Returns the color of the {@link Job}.
+     *
+     * @return the color
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Setter to set the color of the {@link Job}.
+     *
+     * @param color the color
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * Returns the {@link Build}s of the {@link Job}.
+     *
+     * @return the {@link Build}s
+     */
     public List<Build> getBuilds() {
         return builds;
     }
 
+    /**
+     * Setter to set the {@link Build}s of the {@link Job}
+     *
+     * @param builds the {@link Build}s
+     */
     public void setBuilds(List<Build> builds) {
         this.builds = builds;
     }
 
+    /**
+     * Returns the last build status of the {@link Job}.
+     *
+     * @return the last build status
+     */
     public String getLastBuildStatus() {
         return lastBuildStatus;
     }
 
+    /**
+     * Setter to set the last build status of the {@link Job}.
+     *
+     * @param lastBuildStatus the last build status
+     */
     public void setLastBuildStatus(String lastBuildStatus) {
         this.lastBuildStatus = lastBuildStatus;
     }
 
+    /**
+     * Returns the id of the {@link Job}.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter to set the id of the {@link Job}.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Adds a given {@link Build} to the {@link Job}.
+     *
+     * @param build the {@link Build}
+     * @return the added {@link Build}
+     */
     public Build addBuild(Build build) {
         getBuilds().add(build);
         build.setJob(this);
--- src/main/java/edu/hm/hafner/warningsngui/service/dto/Result.java
+++ src/main/java/edu/hm/hafner/warningsngui/service/dto/Result.java
@@ -6,6 +6,9 @@ import edu.hm.hafner.analysis.Report;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * DTO that represents a {@link Result}.
+ */
 public class Result {
     private int id;
     private String warningId;
@@ -23,6 +26,30 @@ public class Result {
     private Report newIssues;
     private Report fixedIssues;
 
+    /**
+     * Creates a new instance of a {@link Result}.
+     */
+    public Result() {
+        this.build = new Build();
+        this.errorMessages = new ArrayList<>();
+        this.infoMessages = new ArrayList<>();
+        this.outstandingIssues = new Report();
+        this.newIssues = new Report();
+        this.fixedIssues = new Report();
+    }
+
+    /**
+     * Creates a new instance of a {@link Result}.
+     *
+     * @param id the id of the {@link Result}
+     * @param warningId the waring id of the {@link Result}
+     * @param latestUrl the latest url of the {@link Result}
+     * @param name the name of the {@link Result}
+     * @param fixedSize the fixed size of the {@link Result}
+     * @param newSize the new size of the {@link Result}
+     * @param totalSize the total size of the {@link Result}
+     * @param qualityGateStatus the quality gate of the {@link Result}
+     */
     public Result(int id, String warningId, String latestUrl, String name, int fixedSize, int newSize, int totalSize, String qualityGateStatus) {
         this.id = id;
         this.warningId = warningId;
@@ -40,15 +67,11 @@ public class Result {
         this.fixedIssues = new Report();
     }
 
-    public Result() {
-        this.build = new Build();
-        this.errorMessages = new ArrayList<>();
-        this.infoMessages = new ArrayList<>();
-        this.outstandingIssues = new Report();
-        this.newIssues = new Report();
-        this.fixedIssues = new Report();
-    }
-
+    /**
+     * Returns the waring id of the {@link Result}.
+     *
+     * @return the warning id
+     */
     public String getWarningId() {
         return warningId;
     }
@@ -57,38 +80,83 @@ public class Result {
         this.warningId = warningId;
     }
 
+    /**
+     * Returns the latest url of the {@link Result}.
+     *
+     * @return the latest url
+     */
     public String getLatestUrl() {
         return latestUrl;
     }
 
+    /**
+     * Setter to set the latest url of the {@link Result}.
+     *
+     * @param latestUrl the url
+     */
     public void setLatestUrl(String latestUrl) {
         this.latestUrl = latestUrl;
     }
 
+    /**
+     * Returns the {@link Report} with outstanding issues of the {@link Result}.
+     *
+     * @return the {@link Report}
+     */
     public Report getOutstandingIssues() {
         return outstandingIssues;
     }
 
+    /**
+     * Setter to set a {@link Report} with outstanding issues of the {@link Result}.
+     *
+     * @param outstandingIssues the {@link Report}
+     */
     public void setOutstandingIssues(Report outstandingIssues) {
         this.outstandingIssues = outstandingIssues;
     }
 
+    /**
+     * Returns the {@link Report} with new issues of the {@link Result}.
+     *
+     * @return the {@link Report}
+     */
     public Report getNewIssues() {
         return newIssues;
     }
 
+    /**
+     * Setter to set a {@link Report} with new issues of the {@link Result}.
+     *
+     * @param newIssues the {@link Report}
+     */
     public void setNewIssues(Report newIssues) {
         this.newIssues = newIssues;
     }
 
+    /**
+     * Returns the {@link Report} with fixed issues of the {@link Result}.
+     *
+     * @return the {@link Report}
+     */
     public Report getFixedIssues() {
         return fixedIssues;
     }
 
+    /**
+     * Setter to set a {@link Report} with fixed issues of the {@link Result}.
+     *
+     * @param fixedIssues the {@link Report}
+     */
     public void setFixedIssues(Report fixedIssues) {
         this.fixedIssues = fixedIssues;
     }
 
+    /**
+     * Returns the name of the {@link Result}.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
@@ -97,66 +165,146 @@ public class Result {
         this.name = name;
     }
 
+    /**
+     * Returns the fixed size of the {@link Result}.
+     *
+     * @return the fixed size
+     */
     public int getFixedSize() {
         return fixedSize;
     }
 
+    /**
+     * Setter to set the fixed size of the {@link Result}.
+     *
+     * @param fixedSize the fixed size
+     */
     public void setFixedSize(int fixedSize) {
         this.fixedSize = fixedSize;
     }
 
+    /**
+     * Returns the new size of the {@link Result}.
+     *
+     * @return the new size
+     */
     public int getNewSize() {
         return newSize;
     }
 
+    /**
+     * Setter to set the new size of the {@link Result}.
+     *
+     * @param newSize the new size
+     */
     public void setNewSize(int newSize) {
         this.newSize = newSize;
     }
 
+    /**
+     * Returns the quality gate status of the {@link Result}.
+     *
+     * @return the quality gate status
+     */
     public String getQualityGateStatus() {
         return qualityGateStatus;
     }
 
+    /**
+     * Setter to set the quality gate status of the {@link Result}.
+     *
+     * @param qualityGateStatus the quality gate status
+     */
     public void setQualityGateStatus(String qualityGateStatus) {
         this.qualityGateStatus = qualityGateStatus;
     }
 
+    /**
+     * Returns the total size of the {@link Result}.
+     *
+     * @return the total size
+     */
     public int getTotalSize() {
         return totalSize;
     }
 
+    /**
+     * Setter to set the total size of the {@link Result}.
+     *
+     * @param totalSize the total size
+     */
     public void setTotalSize(int totalSize) {
         this.totalSize = totalSize;
     }
 
+    /**
+     * Returns the {@link Build} of the {@link Result}.
+     *
+     * @return the {@link Build}
+     */
     public Build getBuild() {
         return build;
     }
 
+    /**
+     * Setter to set the {@link Build} of the {@link Result}.
+     *
+     * @param build the {@link Build}
+     */
     public void setBuild(Build build) {
         this.build = build;
     }
 
+    /**
+     * Returns the error messages of the {@link Result}.
+     *
+     * @return the error messages
+     */
     public List<String> getErrorMessages() {
         return errorMessages;
     }
 
+    /**
+     * Setter to set the error messages of the {@link Result}.
+     *
+     * @param errorMessages the error messages
+     */
     public void setErrorMessages(List<String> errorMessages) {
         this.errorMessages = errorMessages;
     }
 
+    /**
+     * Returns the info messages of the {@link Result}.
+     *
+     * @return the info messages
+     */
     public List<String> getInfoMessages() {
         return infoMessages;
     }
 
+    /**
+     * Setter to set the info messages of the {@link Result}.
+     *
+     * @param infoMessages the info messages
+     */
     public void setInfoMessages(List<String> infoMessages) {
         this.infoMessages = infoMessages;
     }
 
+    /**
+     * Returns the id of the {@link Result}.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter to set the id of the {@link Result}.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
